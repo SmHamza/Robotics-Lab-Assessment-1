9ad6f75a3ed9ba955e5dcd76568ecca6d036ea7e
Syed Muhammad Hamza <52901691+SmHamza@users.noreply.github.com>
2021-04-02 22:56:30 +1100

    Create py_hd_pub.py

---
+++ py_hd_pub.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.location = str(input("Location: "))
+        self.publisher_ = self.create_publisher(String, 'weather/requests', 10)
+        timer_period = 1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = self.location
+        self.publisher_.publish(msg)
+        # self.get_logger().info('Publishing: "%s"' % msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
